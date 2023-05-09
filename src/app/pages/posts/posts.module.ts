d5448cc41d605b4febbfc1920819af2c8ce02fff
Shahrul Haniff <shahrul941013@gmail.com>
2023-05-09 18:50:19 +0800

    asd

--- src/app/pages/posts/posts.module.ts
+++ src/app/pages/posts/posts.module.ts
@@ -2,10 +2,6 @@ import { PostsComponent } from './posts.component';
 import { PostsRoutingModule } from './posts-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-// import { ApiService } from '../../services/api/api.service';
-// import { HttpClientModule } from '@angular/common/http';
-
-
 
 @NgModule({
   declarations: [
@@ -13,9 +9,7 @@ import { CommonModule } from '@angular/common';
   ],
   imports: [
     CommonModule,
-    PostsRoutingModule,
-    // HttpClientModule
-  ],
-  // providers: [ApiService],
+    PostsRoutingModule
+  ]
 })
 export class PostsModule { }
