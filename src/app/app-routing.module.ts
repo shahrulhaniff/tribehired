04ab8a997bc9bba6602e5aec32d6a35abf53421a
Shahrul Haniff <shahrul941013@gmail.com>
2023-05-09 16:16:50 +0800

    route config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
+import { PostsComponent } from './pages/posts/posts.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "posts",
+    // canActivate: [AuthenticationService],
+    loadChildren: () =>
+    import("./pages/posts/posts.module").then((m) => m.PostsModule),
+  },
+  {
+    path: 'posts-parent',
+    component: PostsComponent,
+    // canActivate: [AuthGuardService],
+    children: [
+      {
+        path: "posts-child",
+        // canActivate: [GuardService],
+        loadChildren: () =>
+          import("./pages/posts/posts.module").then((m) => m.PostsModule),
+      },
+	    {
+        path: '',
+        redirectTo: 'posts',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  { path: '', redirectTo: 'posts' },
+  { path: '**', redirectTo: 'posts' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
