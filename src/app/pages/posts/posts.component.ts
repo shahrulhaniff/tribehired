c9a2e28039bc8eacb714f8428240c5332841ae6b
Shahrul Haniff <shahrul941013@gmail.com>
2023-05-09 18:48:42 +0800

    remove console

--- src/app/pages/posts/posts.component.ts
+++ src/app/pages/posts/posts.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../services/api/api.service';
 import { lastValueFrom } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
-
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
@@ -20,24 +19,16 @@ export class PostsComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-
     const params = this.route.snapshot.params['id'];
     this.fetchData(params);
   }
 
   async fetchData( id: any) {
-    console.log(id)
-
     var apiUrl = this.api.sitelink + 'posts';
-
     if(id!=undefined){
       apiUrl = this.api.sitelink + 'posts/' + id;
-      console.log(apiUrl);
-
       this.getComments(id);
     }
-
-
     const res: any = await lastValueFrom(this.api.get(apiUrl));
     console.log(Array.isArray(res));
     if (Array.isArray(res)) { this.posts = res; }
@@ -45,17 +36,9 @@ export class PostsComponent implements OnInit {
     console.log(this.posts);
   }
 
-  async getComments(cid:any){
-    let commentApi = this.api.sitelink + 'comments?postId=' + cid;
-    const comments: any = await lastValueFrom(this.api.get(commentApi));
-    this.comments = comments;
-    console.log(comments);
+  async getComments(id:any){
+    let commentApi = this.api.sitelink + 'comments?postId=' + id;
+    this.comments = await lastValueFrom(this.api.get(commentApi));
+    console.table(this.comments);
   }
 }
-
-
-
-/*
-Side Note Update
-// const res : any = await this.api.get(this.api.sitelink + 'posts').toPromise(); // no longer use on latest angular version rxjsv7
-*/
